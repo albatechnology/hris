da4f4f18543688c6398d225954167aaa58eb8e9a
Alba Tech <60589540+poedihippo@users.noreply.github.com>
2024-02-21 15:39:30 +0700

    Update 2024_01_23_095831_create_user_educations_table.php

--- database/migrations/2024_01_23_095831_create_user_educations_table.php
+++ database/migrations/2024_01_23_095831_create_user_educations_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained();
             $table->string('type');
-            $table->string('level');
+            $table->string('level')->nullable();
             $table->string('name')->nullable();
             $table->string('institution_name');
             $table->string('majors')->nullable();
