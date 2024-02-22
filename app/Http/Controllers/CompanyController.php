48899b1f3b4c35263e39bedbc4c2c991bb16718b
Rinaaa22 <155939603+Rinaaa22@users.noreply.github.com>
2024-02-22 17:40:05 +0700

    [ADD]  Dashboard CRUD Company

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Company\MassDestroyRequest;
+use App\Http\Requests\Company\StoreRequest;
+use App\Http\Requests\Company\UpdateRequest;
+use App\Models\Company;
+use App\View\Components\Datatables\DatatableAction;
+use Exception;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Hash;
+use DataTables;
+
+class CompanyController extends Controller
+{
+    // constructor
+    public function __construct(public Company $model)
+    {
+        $this->middleware('permission:company_access', ['only' => ['index', 'show']]);
+        $this->middleware('permission:company_create', ['only' => ['create', 'store']]);
+        $this->middleware('permission:company_edit', ['only' => ['edit', 'update']]);
+        $this->middleware('permission:company_delete', ['only' => ['destroy', 'massDestroy']]);
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request): mixed
+    {
+        if ($request->ajax()) {
+            $query = $this->model->query();
+            $table = DataTables::of($query);
+
+            $table->addColumn('checkbox', '&nbsp;');
+
+            $table->editColumn('actions', function ($row) {
+                return Blade::renderComponent(new DatatableAction(
+                    showRoute: route('companies.show', $row->id),
+                    editRoute: route('companies.edit', $row->id),
+                    destroyRoute: route('companies.destroy', $row->id),
+                    access: 'company_access',
+                ));
+            });
+
+            $table->rawColumns(['checkbox', 'actions']);
+
+            return $table->make(true);
+        }
+
+        return view('companies.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): View
+    {
+        return view('companies.create', [
+            'model' => $this->model,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRequest $request): RedirectResponse
+    {
+        try {
+            $this->model->create($request->all());
+
+            $alert['success'] = self::CREATED_MESSAGE;
+        } catch (Exception $e) {
+            $alert['error'] = $e->getMessage();
+        }
+
+        return to_route('companies.index')->with(key($alert), current($alert));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Company $company): View
+    {
+        return view('companies.show', [
+            'model' => $company,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Company $company): View
+    {
+        return view('companies.edit', [
+            'model' => $company,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRequest $request, $id)
+    {
+        try {
+            $company = Company::findOrFail($id);
+            $company->update($request->all());
+            $company->syncPermissions(array_map('intval', $request->permission_ids ?? []));
+
+            $alert['success'] = self::UPDATED_MESSAGE;
+        } catch (\Exception $th) {
+            $alert['error'] = $th->getMessage();
+        }
+
+        return to_route('companies.index')->with(key($alert), current($alert));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Company $company): RedirectResponse
+    {
+        try {
+            $company->delete();
+
+            $alert['success'] = self::DELETED_MESSAGE;
+        } catch (Exception $e) {
+            $alert['error'] = $e->getMessage();
+        }
+
+        return to_route('companies.index')->with(key($alert), current($alert));
+    }
+
+    /**
+     * Remove the mass resource from storage.
+     */
+    public function massDestroy(MassDestroyRequest $request): JsonResponse
+    {
+        try {
+            $this->model->whereIn('id', $request->ids)->delete();
+
+            $alert['success'] = self::DELETED_MESSAGE;
+        } catch (Exception $e) {
+            $alert['error'] = $e->getMessage();
+        }
+
+        return $this->jsonResponse(key($alert), current($alert));
+    }
+}
