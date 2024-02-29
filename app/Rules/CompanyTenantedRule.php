3d86316f99d1de78a710757be4713d279afbc4f9
Alba Tech <60589540+poedihippo@users.noreply.github.com>
2024-02-29 15:48:37 +0700

    Update CompanyTenantedRule.php

--- app/Rules/CompanyTenantedRule.php
+++ app/Rules/CompanyTenantedRule.php
@@ -7,8 +7,11 @@ use Illuminate\Contracts\Validation\ValidationRule;
 
 class CompanyTenantedRule implements ValidationRule
 {
-    public function __construct(private $model = null, private string $message = 'Company not found', private ?Closure $function = null)
-    {
+    public function __construct(
+        private $model = null,
+        private string $message = 'Company not found',
+        private ?Closure $query = null
+    ) {
         if (is_null($model)) {
             $this->model = \App\Models\Company::class;
         }
@@ -22,7 +25,7 @@ class CompanyTenantedRule implements ValidationRule
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
         $company = $this->model::tenanted()
-            ->when($this->function, $this->function)
+            ->when($this->query, $this->query)
             ->firstWhere('id', $value);
 
         if (!$company) {
