aa24aa8948ac872359d128c790fcca6526c1f017
Muhammad Robbi Zulfikar <60589540+poedihippo@users.noreply.github.com>
2024-04-02 14:51:50 +0700

    [fix] payroll schedule + prorate

--- app/Http/Controllers/Api/PayrollProrateController.php
+++ app/Http/Controllers/Api/PayrollProrateController.php
@@ -14,17 +14,23 @@ class PayrollProrateController extends BaseController
     {
         parent::__construct();
 
-        $this->middleware('permission:payroll_prorate_read', ['only' => 'index']);
-        $this->middleware('permission:payroll_prorate_edit', ['only' => 'update']);
+        $this->middleware('permission:payroll_setting_read', ['only' => 'index']);
+        $this->middleware('permission:payroll_setting_edit', ['only' => 'update']);
     }
 
     public function index(Request $request)
     {
-        return new PayrollProrateResource($request);
+        $request->validate([
+            'filter.company_id' => 'required',
+        ]);
+
+        $payrollSetting = PayrollSetting::tenanted()->where('company_id', $request->filter['company_id'])->firstOrFail();
+        return new PayrollProrateResource($payrollSetting);
     }
 
-    public function update(PayrollSetting $payrollSetting, UpdateRequest $request)
+    public function update(UpdateRequest $request)
     {
+        $payrollSetting = PayrollSetting::where('company_id', $request->company_id)->firstOrFail();
         $payrollSetting->update($request->validated());
 
         return (new PayrollProrateResource($payrollSetting))->response()->setStatusCode(Response::HTTP_ACCEPTED);
--- app/Http/Controllers/Api/PayrollScheduleController.php
+++ app/Http/Controllers/Api/PayrollScheduleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\Api\PayrollSchedule\UpdateRequest;
 use App\Http\Resources\DefaultResource;
+use App\Http\Resources\PayrollSchedule\PayrollScheduleResource;
 use App\Models\PayrollSetting;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -14,17 +15,23 @@ class PayrollScheduleController extends BaseController
     {
         parent::__construct();
 
-        $this->middleware('permission:payroll_schedule_read', ['only' => 'index']);
-        $this->middleware('permission:payroll_schedule_edit', ['only' => 'update']);
+        $this->middleware('permission:payroll_setting_read', ['only' => 'index']);
+        $this->middleware('permission:payroll_setting_edit', ['only' => 'update']);
     }
 
     public function index(Request $request)
     {
-        return new PayrollScheduleResource($request);
+        $request->validate([
+            'filter.company_id' => 'required',
+        ]);
+
+        $payrollSetting = PayrollSetting::tenanted()->where('company_id', $request->filter['company_id'])->firstOrFail();
+        return new PayrollScheduleResource($payrollSetting);
     }
 
-    public function update(PayrollSetting $payrollSetting, UpdateRequest $request)
+    public function update(UpdateRequest $request)
     {
+        $payrollSetting = PayrollSetting::where('company_id', $request->company_id)->firstOrFail();
         $payrollSetting->update($request->validated());
 
         return (new PayrollScheduleResource($payrollSetting))->response()->setStatusCode(Response::HTTP_ACCEPTED);
