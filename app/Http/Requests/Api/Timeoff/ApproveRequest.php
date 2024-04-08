94cd598c453b4f1368a92380cbb94b94cc3d6482
Alba Tech <60589540+poedihippo@users.noreply.github.com>
2024-04-08 15:59:27 +0700

    Update ApproveRequest.php

--- app/Http/Requests/Api/Timeoff/ApproveRequest.php
+++ app/Http/Requests/Api/Timeoff/ApproveRequest.php
@@ -3,32 +3,17 @@
 namespace App\Http\Requests\Api\Timeoff;
 
 use App\Enums\ApprovalStatus;
-use App\Traits\Requests\RequestToBoolean;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
 class ApproveRequest extends FormRequest
 {
-    use RequestToBoolean;
-
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
         return true;
-    }
-
-    /**
-     * Prepare inputs for validation.
-     *
-     * @return void
-     */
-    protected function prepareForValidation()
-    {
-        $this->merge([
-            'approval_status' => is_null($this->approval_status) ? null : $this->toBoolean($this->approval_status),
-        ]);
     }
 
     /**
