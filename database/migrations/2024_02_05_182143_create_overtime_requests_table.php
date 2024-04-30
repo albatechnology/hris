cec268f887c3ca14881ce9a08c5b76a0ed389889
Alba Tech <60589540+poedihippo@users.noreply.github.com>
2024-04-30 10:09:51 +0700

    Update 2024_02_05_182143_create_overtime_requests_table.php

--- database/migrations/2024_02_05_182143_create_overtime_requests_table.php
+++ database/migrations/2024_02_05_182143_create_overtime_requests_table.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Enums\ApprovalStatus;
-use App\Enums\OvertimeRequestType;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
