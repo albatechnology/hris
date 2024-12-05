ee243f7d073236fc330c31d7053417f95ef236c8
Alba Tech <60589540+poedihippo@users.noreply.github.com>
2024-12-05 17:33:21 +0700

    Update AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,7 +25,7 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Gate::before(function ($user, $ability) {
-            return $user->type->is(UserType::SUPER_ADMIN) ? true : null;
+            return $user->type->in([UserType::SUPER_ADMIN, UserType::ADMIN]) ? true : null;
         });
     }
 }
