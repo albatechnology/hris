ef32bf838290f0d6b45818350809e80262aa6537
Nikko Fe <febika.nikko@gmail.com>
2025-02-03 10:06:29 +0700

    Update TimeoffQuotaController.php

--- app/Http/Controllers/Api/TimeoffQuotaController.php
+++ app/Http/Controllers/Api/TimeoffQuotaController.php
@@ -127,6 +127,7 @@ class TimeoffQuotaController extends BaseController
         $data = QueryBuilder::for(
             User::select(['id', 'branch_id', 'name', 'last_name', 'nik'])
                 ->tenanted()
+                ->with('branch', fn($q) => $q->select('id', 'name'))
             // ->with('timeoffQuotas', function ($q) {
             //     $q->whereActive()
             //         ->select('user_id', 'timeoff_policy_id', DB::raw('SUM(quota) as total_quota'))
