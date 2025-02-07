5a252b3888fd3fe15f648cd9b0c792b935ee3e13
Nikko Fe <febika.nikko@gmail.com>
2025-02-07 18:53:39 +0700

    Update RunPayrollController.php

--- app/Http/Controllers/Api/RunPayrollController.php
+++ app/Http/Controllers/Api/RunPayrollController.php
@@ -154,7 +154,7 @@ class RunPayrollController extends BaseController
                 $q->select('id', 'nik', 'name', 'last_name', 'company_id', 'branch_id', 'join_date', 'resign_date')
                     ->with('branch', fn($q) => $q->select('id', 'name'))
                     ->with('payrollInfo', function ($q) {
-                        $q->select('user_id', 'bank_name', 'bank_account_no', 'bank_account_holder', 'secondary_bank_name', 'secondary_bank_account_no', 'secondary_bank_account_holder', 'currency')
+                        $q->select('user_id', 'bank_id', 'bank_name', 'bank_account_no', 'bank_account_holder', 'secondary_bank_name', 'secondary_bank_account_no', 'secondary_bank_account_holder', 'currency')
                             ->with('bank');
                     })
                     ->with('positions', fn($q) => $q->select('user_id', 'position_id')
