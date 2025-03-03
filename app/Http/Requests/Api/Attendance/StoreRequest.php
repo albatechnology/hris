e5b5869d2114df9d6f0505c67638b84822cadc8a
Nikko Fe <febika.nikko@gmail.com>
2025-03-03 18:37:21 +0700

    [FIX] schedule & shift with trashed on attendance

--- app/Http/Requests/Api/Attendance/StoreRequest.php
+++ app/Http/Requests/Api/Attendance/StoreRequest.php
@@ -42,9 +42,9 @@ class StoreRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'schedule_id' => ['required', new CompanyTenantedRule(Schedule::class, 'Schedule not found')],
+            'schedule_id' => ['required', new CompanyTenantedRule(Schedule::class, 'Schedule not found', fn($q) => $q->withTrashed())],
             // 'shift_id' => ['required', new CompanyTenantedRule(Shift::class, 'Shift not found')],
-            'shift_id' => ['required', new CompanyTenantedRule(Shift::class, 'Shift not found', fn($q) => $q->orWhereNull('company_id'))],
+            'shift_id' => ['required', new CompanyTenantedRule(Shift::class, 'Shift not found', fn($q) => $q->withTrashed()->orWhereNull('company_id'))],
             'is_clock_in' => 'required|boolean',
             'time' => 'required|date_format:Y-m-d H:i:s',
             'type' => ['required', Rule::enum(AttendanceType::class)],
