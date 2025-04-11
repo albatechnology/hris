785b1f7a78f694db604a3d01f697d762b1d727a9
Nikko Fe <febika.nikko@gmail.com>
2025-04-11 11:13:13 +0700

    Update ResignationReason.php

--- app/Enums/ResignationReason.php
+++ app/Enums/ResignationReason.php
@@ -6,5 +6,10 @@ enum ResignationReason: string
 {
     use BaseEnum;
 
-    case RESIGN = 'resign';
+    case RESIGNED_VOLUNTARILY_WITHOUT_PRESSURE_IN_COMPLIANCE = 'Resigned voluntarily without pressure, in compliance with company regulations';
+    case RESIGNED_VOLUNTARILY_WITHOUT_PRESSURE_NOT_IN_COMPLIANCE = 'Resigned voluntarily without pressure, not in compliance with company regulations';
+    case DID_NOT_PASS_PROBATION_PERIOD = 'Did not pass the probation period';
+    case COMPLETION_OF_FIXED_TERM_AGREEMENT = 'Completion of a fixed-term employment agreement (PKWT)';
+    case PASSED_AWAY = 'Employee passed away';
+    case REACHES_RETIREMENT_AGE = 'Employee reaches retirement age';
 }
