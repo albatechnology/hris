2788123c0e88994febc45eb363501d5217e46eae
poedihippo <60589540+poedihippo@users.noreply.github.com>
2025-04-27 11:49:51 +0700

    update enum JkkTier

--- app/Enums/JkkTier.php
+++ app/Enums/JkkTier.php
@@ -9,8 +9,8 @@ enum JkkTier: int
     case VERY_LOW = 24;
     case LOW = 54;
     case MEDIUM = 89;
-    case HIGH = 27;
-    case VERY_HIGH = 74;
+    case HIGH = 127;
+    case VERY_HIGH = 174;
 
     public function getValue(): float
     {
