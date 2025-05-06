c4a94e28e77f06b0e2e8c9b3fbe5684beaec5e2f
Nikko Fe <41881787+nikkofebika@users.noreply.github.com>
2025-05-06 17:20:13 +0700

    Update AbsenceReminderController.php

--- app/Http/Controllers/Api/AbsenceReminderController.php
+++ app/Http/Controllers/Api/AbsenceReminderController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\Api\AbsenceReminder\UpdateRequest;
 use App\Http\Resources\DefaultResource;
+use App\Mail\TestEmail;
 use App\Models\AbsenceReminder;
+use App\Models\User;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Mail;
 use Spatie\QueryBuilder\AllowedFilter;
 use Spatie\QueryBuilder\QueryBuilder;
 
@@ -20,6 +23,7 @@ class AbsenceReminderController extends BaseController
 
     public function index()
     {
+        Mail::to(User::find(20))->send(new TestEmail());
         $data = QueryBuilder::for(AbsenceReminder::tenanted())
             ->allowedFilters([
                 AllowedFilter::exact('company_id'),
