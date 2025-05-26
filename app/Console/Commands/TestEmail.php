88870a850c936e86d69166a9b8584a7cee5ec0b7
Nikko Fe <41881787+nikkofebika@users.noreply.github.com>
2025-05-26 16:49:21 +0700

    Create TestEmail.php

---
+++ app/Console/Commands/TestEmail.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Mail\TestEmail as MailTestEmail;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class TestEmail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:test-email {emails?*}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Mail::sendNow($this->argument('emails'), new MailTestEmail());
+    }
+}
