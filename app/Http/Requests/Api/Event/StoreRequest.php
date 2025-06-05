37ae4c71a5888c77ddf977d37d18ffc39716f484
Nikko Fe <41881787+nikkofebika@users.noreply.github.com>
2025-06-05 13:06:41 +0700

    Update StoreRequest.php

--- app/Http/Requests/Api/Event/StoreRequest.php
+++ app/Http/Requests/Api/Event/StoreRequest.php
@@ -19,6 +19,23 @@ class StoreRequest extends FormRequest
     }
 
     /**
+     * Prepare inputs for validation.
+     *
+     * @return void
+     */
+    protected function prepareForValidation()
+    {
+        $companyId = $this->company_id ?? null;
+        if ($this->branch_id) {
+            $companyId = Branch::tenanted()->where('id', $this->branch_id)->firstOrFail(['company_id'])->company_id;
+        }
+
+        $this->merge([
+            'company_id' => $companyId
+        ]);
+    }
+
+    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
