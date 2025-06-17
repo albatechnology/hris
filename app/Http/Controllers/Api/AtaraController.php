24bd8d9227b0059a55abdfab5d6789e092e7ff56
Nikko Fe <41881787+nikkofebika@users.noreply.github.com>
2025-06-17 17:34:35 +0700

    Update AtaraController.php

--- app/Http/Controllers/Api/AtaraController.php
+++ app/Http/Controllers/Api/AtaraController.php
@@ -10,7 +10,7 @@ class AtaraController extends BaseController
 {
     public function contact(ContactRequest $request)
     {
-        Mail::to('info@atara.id')->bcc(['poedi@albatech.id', 'albaprogrammer2@gmail.com'])->sendNow(new AtaraContactMail($request));
+        Mail::to('info@atara.id')->bcc(['poedi@albatech.id', 'albaprogrammer2@gmail.com'])->send(new AtaraContactMail($request));
 
         return $this->okResponse();
     }
