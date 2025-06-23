aa3b6f031193093dd8bcb7155400b7687fa2f8ae
Nikko Fe <41881787+nikkofebika@users.noreply.github.com>
2025-06-23 16:18:57 +0700

    [FIX] RelationshipType

--- app/Enums/RelationshipType.php
+++ app/Enums/RelationshipType.php
@@ -4,16 +4,9 @@ namespace App\Enums;
 
 enum RelationshipType: string
 {
-    use BaseEnum;
-
     case FATHER = 'father';
     case MOTHER = 'mother';
-    case BROTHER = 'brother';
-    case GRANDFATHER = 'grandfather';
-    case GRANDMOTHER = 'grandmother';
-    case UNCLE = 'uncle';
-    case AUNT = 'aunt';
-    case COUSIN = 'cousin';
-    case FRIEND = 'friend';
+    case COUSIN = 'spouse';
+    case FRIEND = 'sibling';
     case OTHER = 'other';
 }
