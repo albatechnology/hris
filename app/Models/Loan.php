3eeb94031b90aa1d508650b339107547fc4b84da
Nikko Fe <41881787+nikkofebika@users.noreply.github.com>
2025-07-04 10:41:44 +0700

    Update Loan.php

--- app/Models/Loan.php
+++ app/Models/Loan.php
@@ -3,14 +3,16 @@
 namespace App\Models;
 
 use App\Enums\LoanType;
+use App\Interfaces\TenantedInterface;
 use App\Traits\Models\CreatedUpdatedInfo;
 use App\Traits\Models\CustomSoftDeletes;
 use App\Traits\Models\TenantedThroughUser;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Loan extends BaseModel
+class Loan extends BaseModel implements TenantedInterface
 {
-    use CustomSoftDeletes, CreatedUpdatedInfo, TenantedThroughUser;
+    use CustomSoftDeletes, CreatedUpdatedInfo;
 
     protected $fillable = [
         'user_id',
@@ -26,6 +28,29 @@ class Loan extends BaseModel
     protected $casts = [
         'type' => LoanType::class,
     ];
+
+    public function scopeTenanted(Builder $query, ?User $user = null): Builder
+    {
+        if (!$user) {
+            /** @var User $user */
+            $user = auth('sanctum')->user();
+        }
+
+        if ($user->is_super_admin) return $query;
+
+        $companyIds = $user->companies()->get(['company_id'])?->pluck('company_id') ?? [];
+        return $query->whereHas('user', fn($q) => $q->whereIn('company_id', $companyIds));
+    }
+
+    public function scopeFindTenanted(Builder $query, int|string $id, bool $fail = true): self
+    {
+        $query->tenanted()->where('id', $id);
+        if ($fail) {
+            return $query->firstOrFail();
+        }
+
+        return $query->first();
+    }
 
     public function details(): HasMany
     {
