5cdd003d7f2cdeca0918a807a31a2c47b163c0b5
Nikko Fe <41881787+nikkofebika@users.noreply.github.com>
2025-09-08 17:55:24 +0700

    Update UserPatrolTask.php

--- app/Models/UserPatrolTask.php
+++ app/Models/UserPatrolTask.php
@@ -85,7 +85,7 @@ class UserPatrolTask extends BaseModel implements HasMedia, TenantedInterface
     public function registerMediaConversions(?\Spatie\MediaLibrary\MediaCollections\Models\Media $media = null): void
     {
         $this->addMediaConversion('thumb')
-            ->fit(\Spatie\Image\Enums\Fit::Max, 200, 200)
+            ->fit(\Spatie\Image\Enums\Fit::Max, 160, 160)
             ->quality(100)
             ->nonOptimized()
             ->queued();
