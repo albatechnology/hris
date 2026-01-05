915a3f70ba593631c32d1eb3b798e63b03d08ab4
poedihippo <febika.nikko@gmail.com>
2026-01-05 12:16:35 +0700

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
 
-        $schedule->job(new \App\Jobs\AnnualLeave\NewEmployee)->monthlyOn(5, '12:12');
+        $schedule->job(new \App\Jobs\AnnualLeave\NewEmployee)->monthlyOn(5, '12:18');
         // $schedule->job(new \App\Jobs\AnnualLeave\NewEmployeeCharyuning)->dailyAt('18:06');
         $schedule->job(new \App\Jobs\UserTransfer\ExecuteUserTransfer)->dailyAt('02:00');
         $schedule->job(new AbsenceReminderBatch())->everyFiveMinutes()->when(fn () => config('app.name') === 'SYNTEGRA');
